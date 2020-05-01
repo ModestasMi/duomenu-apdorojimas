2abb60bd82b6d900e3718bafef04e113d4c77fb0
ModestasMi <63955166+ModestasMi@users.noreply.github.com>
2020-05-01 14:49:00 +0300

    Studentas
    Studento duomenys

---
+++ Studentas.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace duomenuapdorojimas
+
+   public class Studentas 
+    {
+        public string vard;
+        public string pav;
+        List<double> pazymys = new List<double>();
+        public double egpazymys;
+        
+   public Studentas(string vardas, string pavarde)
+        {
+            vard = vardas;
+            pav = pavarde;
+        }
+
+        public Studentas(string vardas, string pavarde, List<double> ivertinimai)
+        {
+            pazymys = ivertinimai;
+        }
+
+        public Studentas(string vardas, string pavarde, List<double> ivertinimai, double egzGrade) : this(vardas, pavarde, ivertinimai)
+        {
+            egpazymys = egzGrade;
+        }
+     }
