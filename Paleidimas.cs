c42a6e4742f7c0096d08087afb8e11ea670e161a
ModestasMi <63955166+ModestasMi@users.noreply.github.com>
2020-05-02 17:16:42 +0300

    failo nuskaitymo parinktis prideta i meniu

--- Paleidimas.cs
+++ Paleidimas.cs
@@ -1,21 +1,30 @@
 using System;
 
+
 namespace duomenuapdorojimas
 {
     class Paleidimas
     {
         static void Main(string[] args)
         {
-         while (true){
+            while (true)
+            {
                 Console.WriteLine("Spauskite 'enter' noredami irasyti studenta");
+                Console.WriteLine("Iveskite 'file' norint nuskaityti duomenis is failo");
                 Console.WriteLine("Iveskite 'exit' norint baigti programa");
 
                 var irasas = Console.ReadLine();
-                if (irasas.Equals("exit")){
-                    break; 
+                if (irasas.Equals("exit"))
+                {
+                    break;
                 }
-                else{
-                    Studentas.NaujasIrasas();
+                else if (irasas.Equals("file"))
+                {
+                    break;
+                }
+                else
+                {
+                    StudentuIrasymas.NaujasIrasas();
                 }
             }
         }
