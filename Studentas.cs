e581f416a4e2a7719907195e2a8b41c9a1477284
ModestasMi <63955166+ModestasMi@users.noreply.github.com>
2020-05-02 16:06:50 +0300

    atvaizdavimas

--- Studentas.cs
+++ Studentas.cs
@@ -119,10 +119,12 @@ namespace duomenuapdorojimas
             if (irasas.Equals("1"))
             {
                 Funkcijos choice = Funkcijos.Vidurkis;
+                Atvaizdavimas.AtvaizduotiSarasa(StudentuIrasymas, choice);
             }
             else if (irasas.Equals("2"))
             {
                 Funkcijos choice = Funkcijos.Mediana;
+                Atvaizdavimas.AtvaizduotiSarasa(StudentuIrasymas, choice);
             }
             else 
             {
@@ -132,3 +134,56 @@ namespace duomenuapdorojimas
 
         }
         }
+        public class Atvaizdavimas
+        {
+            public static void AtvaizduotiSarasa(List<Studentas> StudentuIrasymas, Funkcijos choice)
+            {
+                var studentoduomenys = new string[] { "Pavarde", "Vardas" };
+                if (choice == Funkcijos.Vidurkis)
+                {
+                    studentoduomenys = studentoduomenys.Append("Galutinis (Vid.)").ToArray();
+                }
+                else if (choice == Funkcijos.Mediana)
+                {
+                    studentoduomenys = studentoduomenys.Append("Galutinis (Med.)").ToArray();
+                }
+                PridetiEilute(studentoduomenys);
+                Console.WriteLine(Bruksniuoti());
+                foreach (var Studentas in StudentuIrasymas)
+                {
+                   PridetiEilute(Studentas.save(choice));
+                }
+            }
+        public static string Lygiuoti(string input, int width)
+        {
+            input = input.Length > width ? input.Substring(0, width - 3) + "..." : input;
+
+            return input.PadRight(width / 2);
+        }
+
+        public static string Bruksniuoti()
+        {
+            return new string('-', 50);
+        }
+
+        public static string Formatuoti(params string[] columns)
+        {
+            int width = 100 / columns.Length;
+            string studentoduomenys = " ";
+
+            foreach (string column in columns)
+            {
+                studentoduomenys += Lygiuoti(column, width);
+            }
+
+            return studentoduomenys;
+        }
+
+
+        public static void PridetiEilute(params string[] columns)
+        {
+                Console.WriteLine(Formatuoti(columns));
+        }
+        }
+    
+}
