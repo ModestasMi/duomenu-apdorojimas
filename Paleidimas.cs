249a7cea473021274f261ea9add8fa31d6bd2f1a
ModestasMi <63955166+ModestasMi@users.noreply.github.com>
2020-05-02 15:57:57 +0300

    Update Paleidimas.cs

--- Paleidimas.cs
+++ Paleidimas.cs
@@ -7,7 +7,7 @@ namespace duomenuapdorojimas
         static void Main(string[] args)
         {
          while (true){
-                Console.WriteLine("Spauskite bet kuri klavisa norint irasyti studenta, tada spauskite 'enter'");
+                Console.WriteLine("Spauskite 'enter' noredami irasyti studenta");
                 Console.WriteLine("Iveskite 'exit' norint baigti programa");
 
                 var irasas = Console.ReadLine();
