b843da154f83fefe7e6425920f6f81bc0f47ca1a
ModestasMi <63955166+ModestasMi@users.noreply.github.com>
2020-05-02 13:18:46 +0300

    meniu pridejimas

--- Paleidimas.cs
+++ Paleidimas.cs
@@ -6,6 +6,18 @@ namespace duomenuapdorojimas
     {
         static void Main(string[] args)
         {
+         while (true){
+                Console.WriteLine("Spauskite bet kuri klavisa norint irasyti studenta, tada spauskite 'enter'");
+                Console.WriteLine("Iveskite 'exit' norint baigti programa");
+
+                var irasas = Console.ReadLine();
+                if (irasas.Equals("exit")){
+                    break; 
+                }
+                else{
+                    Studentai.NaujasIrasas();
+                }
+            }
         }
     }
 }
