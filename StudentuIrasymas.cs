207bf0f1766c3c985dad1f6cac0a15b1803a132f
ModestasMi <63955166+ModestasMi@users.noreply.github.com>
2020-05-03 14:22:41 +0300

    isskaidymas i .cs failus

---
+++ StudentuIrasymas.cs
@@ -0,0 +1,49 @@
+﻿using duomenuapdorojimas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class StudentuIrasymas
+{
+    public static void NaujasIrasas()
+    {
+        string irasas;
+        var StudentuIrasymas = new List<Studentas>();
+
+        while (true)
+        {
+            Console.WriteLine("Irasykite pavarde, varda ir pazymius, kai baigsite rasyti dar karta spauskite 'enter'");
+            Console.WriteLine("Jei norite generuoti pazymius atsitiktinai, po pavardes ir vardo iveskite norimu generuoti pazymiu skaiciu ir 'k' raide po jo, pvz '5k' ir spauskite 'enter'");
+            irasas = Console.ReadLine();
+
+            if (irasas.Length != 0)
+            {
+                var duomenys = irasas.Split();
+                StudentuIrasymas.Add(new Studentas(irasas));
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Irasykite '1' Vidurkio skaiciavimui, '2' Medianos skaiciavimui");
+        irasas = Console.ReadLine();
+        if (irasas.Equals("1"))
+        {
+            Funkcijos choice = Funkcijos.Vidurkis;
+            Atvaizdavimas.AtvaizduotiSarasa(StudentuIrasymas, choice);
+        }
+        else if (irasas.Equals("2"))
+        {
+            Funkcijos choice = Funkcijos.Mediana;
+            Atvaizdavimas.AtvaizduotiSarasa(StudentuIrasymas, choice);
+        }
+        else
+        {
+            Console.WriteLine("Pasirinkta neteisingai, pradekite is naujo");
+        }
+
+
+    }
+}
